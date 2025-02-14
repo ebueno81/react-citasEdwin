c06e85f1f7427f013bbbbd6ac8b355a4c32c29f8
ebueno81 <ebueno81@hotmail.com>
2025-02-13 22:46:44 -0500

    Descripción de los cambios realizados

--- src/components/FiltroTareas.jsx
+++ src/components/FiltroTareas.jsx
@@ -3,6 +3,14 @@ import React from 'react'
 
 export default function FiltroTareas() {
   return (
-    <div>FiltroTareas</div>
+    <div className='mb-4'>
+        <h1 className='text-large font-bold mb-2'>
+        FiltroTareas
+        </h1>
+        <select className="flex justify-between p-2 bg-gray-100 rounded mb-2"
+        >
+            <option>--Seleccionar Opcion--</option>
+        </select>
+        </div>
   )
 }
--- src/components/ListadoTareas.jsx
+++ src/components/ListadoTareas.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function ListadoTareas() {
+export default function () {
   return (
-    <div>ListadoTareas</div>
+    <div>
+        <h1 className='flex text-xl font-bold mb-4'>Listado de Tareas</h1>
+        <select className="flex justify-between p-2 bg-gray-100 rounded mb-2"
+        >
+            <option>--Seleccionar Opcion--</option>
+        </select>
+    </div>
   )
 }
